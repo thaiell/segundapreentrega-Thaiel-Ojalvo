b699a31f78147d91ee89b87184f5b31020cfbecc
Thaiel <thaielog@gmail.com>
2023-07-11 19:12:33 -0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Cart from "./components/Cart/Cart"
-
+import { CarritoContext } from "./context/CarritoContext"
 
 function App() {
 
 
   return (
     <>
+<CarritoContext>
       <BrowserRouter>
         <NavBar />
 
@@ -19,8 +20,9 @@ function App() {
           <Route path="/item/:idItem" element={ <ItemDetailContainer/> }/>
           <Route path='/cart' element={<Cart/>}></Route>
           <Route path="/checkout" element={<h2>muy pronto</h2>}></Route>
-       </Routes>
+       </Routes> 
       </BrowserRouter>
+     </CarritoContext>
     </>
   )
 }
